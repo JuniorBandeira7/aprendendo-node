027026f59cb3bf68f1a1c373b9b657d68b481d0d
Julio Bandeira <88752929+JuniorBandeira7@users.noreply.github.com>
2024-02-06 17:40:08 -0300

    Update app.js

--- API-REST/src/app.js
+++ API-REST/src/app.js
@@ -11,6 +11,17 @@ const selecoes = [
     {id: 4, selecao: 'holanda', grupo: 'G'}
 ] 
 
+function buscarSelecaoPorId(id){
+    return selecoes.filter(selecao => selecao.id == id)
+}
+//pegar a posicao ou index do elemento no array por id
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
+
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
 
 // criar rota padrão ou raiz
 app.get('/', (req, res) =>{// ver o que é rota no gpt
@@ -24,6 +35,19 @@ app.get('/selecoes', (req, res) => {
 app.post('/selecoes', (req, res) =>{
     selecoes.push(req.body)
     res.status(201).send('selecao cadastrada')
+})
+
+app.delete('/selecoes/:id', (req,res) =>{
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send(`selecao id: ${req.params.id} excluida`)
+})
+
+app.put('/selecoes/:id', (req,res) =>{
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo = req.body.grupo
+    res.json(selecoes)
 })
 
 export default app
