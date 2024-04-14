c23a2fd32a7e862a3017e0115a9e3ad7b4f85677
Julio Bandeira <88752929+JuniorBandeira7@users.noreply.github.com>
2024-04-13 22:26:19 -0300

    aula

---
+++ Aulas/Coore_modules/path/index.js
@@ -0,0 +1,8 @@
+const path = require("path")
+
+const customPath = "/path/path.pdf"
+
+console.log(path.dirname(customPath))
+console.log(path.basename(customPath))
+console.log(path.extname(customPath))
+
