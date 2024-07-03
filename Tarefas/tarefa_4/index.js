94b3fa4366cf09e4962e6981307569a6068ab36e
Julio Bandeira <juliobandeira77@gmail.com>
2024-07-03 15:14:53 -0300

    .

--- Tarefas/tarefa_4/index.js
+++ Tarefas/tarefa_4/index.js
@@ -5,21 +5,34 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) =>{
-    const doces = [
-        {
-            name:"jujuba",
-            preco: 1
-        },
-        {
-            name:"paçoca",
-            preco: 50
-        }
-    ]
+app.use(express.static('public'))
 
-    res.render('home', {doces : doces})
+const doces = [
+    {
+        id: '1',
+        name: 'jujuba',
+        preco: 1
+    },
+    {
+        id: '2',
+        name: 'paçoca',
+        preco: 50
+    }
+]
+
+app.get('/doce/:id', (req, res)=>{
+    const doce = doces[parseInt(req.params.id) - 1]
+
+    res.render('doce' , {doce})
+})
+
+
+app.get('/', (req, res) =>{
+
+    res.render('home', {doces})
 })
 
 app.listen(3000, ()=>{
     console.log('app rodando na rota 3000')
+    
 })
