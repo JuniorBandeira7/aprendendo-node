16e70a24483b8f4d74c7ccf1248af9b35b8c98c4
Julio Bandeira <juliobandeira77@gmail.com>
2024-07-12 17:55:42 -0300

    .

---
+++ Aulas/sequelize/select/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize', 'root', '',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+//try{
+    //sequelize.authenticate()
+   //console.log('conectado')
+//}catch(err){
+    //console.log('não conectou', err)
+//}
+
+module.exports = sequelize
