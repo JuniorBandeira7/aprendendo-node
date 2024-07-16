bd33ae2e1ae5cf35400005ed5a322a4c86f073dc
Julio Bandeira <juliobandeira77@gmail.com>
2024-07-15 22:51:36 -0300

    .

---
+++ Aulas/MVC/rotas/routes/tasksRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const app = express()
+const port = 3000
+
+const TaskController = require('../controllers/TaskController')
+
+router.get('/add', TaskController.createTask)
+router.get('/', TaskController.showTasks)
+
+module.exports = router
+
+
