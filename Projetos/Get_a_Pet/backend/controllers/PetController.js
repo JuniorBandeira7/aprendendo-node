45871d6d30565357e812610babab397435fb0dec
JuniorBandeira7 <juliobandeira77@gmail.com>
2024-08-12 23:54:14 -0300

    .

--- Projetos/Get_a_Pet/backend/controllers/PetController.js
+++ Projetos/Get_a_Pet/backend/controllers/PetController.js
@@ -205,4 +205,48 @@ module.exports = class PetController{
         res.status(200).json({message: 'Pet atualizado com sucesso!'})
     }
 
+    static async schedule(req, res){
+
+        //check if pet exists
+        const id = req.params.id
+
+        const pet = await Pet.findOne({_id: id})
+
+        if(!pet){
+            res.status(404).json({message: 'Pet não encontrado'})
+            return
+        }
+        
+        // check if user registered the pet
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if(pet.user._id.equals(user._id)){
+            res.status(422).json({message: 'Você não pode agendar uma visita com seu proprio pet'})
+            return
+        }
+
+        // check if user has already scheduled a visit
+        if(pet.adopter){
+            if(pet.adopter._id.equals(user._id)){
+                res.status(422).json({message: 'Você já agendou uma visita para este pet'})
+                return
+            }
+        }
+
+        // add user to pet
+        pet.adopter ={
+            _id: user._id,
+            name: user.name,
+            image: user.image
+        }
+
+        await Pet.findOneAndUpdate({ _id: id },  pet);
+
+        res.status(200).json({
+            message: `A visita foi agendada com sucesso, entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}`
+        })
+        return
+    }
+
 }
